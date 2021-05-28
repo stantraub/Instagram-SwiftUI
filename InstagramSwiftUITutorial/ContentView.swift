bd56373af67dbae8862ee634f24ad1f8ac235fae
Stanley <stanley.traub@gmail.com>
2021-05-27 17:36:01 -0700

    Initial Commit

---
+++ InstagramSwiftUITutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramSwiftUITutorial
+//
+//  Created by Stanley Traub on 5/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstagramSwiftUITutorial/InstagramSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUITutorialApp.swift
+//  InstagramSwiftUITutorial
+//
+//  Created by Stanley Traub on 5/27/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
